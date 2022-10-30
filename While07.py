b2fb8f276490ee5facf829b47244f48ee9d96904
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2022-10-30 21:26:19 +0500

    777

--- While07.py
+++ While07.py
@@ -7,11 +7,10 @@ def main(s):
         int: return answer
     """
     i=0
-
     d=0
-    while i<len(s):
-        if i[i]%2==0:
+    while i<=len(s):
+        if i%2!=0:
             d+=1
         i+=1
     return d
-print(main('56786543250'))
+print(main('456777777777'))
