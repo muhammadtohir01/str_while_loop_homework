fd108b6fd93e974b603476e8ba1d8fe7abf16e95
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2022-10-31 10:36:28 +0500

    8m ' '

--- While08.py
+++ While08.py
@@ -6,4 +6,14 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    d=0
+    while i<len(s):
+        if str(s[i]).isdigit():
+         if int(s[i])%2==1:
+            d+=1
+        else:
+            d+=0
+        i+=1
+    return d
+print(main('1567534'))
